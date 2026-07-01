513523b98529eef460b1d3e9046f57be899ea016
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-07-01 17:37:27 +0300

    Update deadline to august 31th

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
 
-    const DeadlineDate = new Date("July 31, 2026 23:59:59").getTime();
+    const DeadlineDate = new Date("August 31, 2026 23:59:59").getTime();
 
     let timeRemaining = DeadlineDate - Date.now();
 
